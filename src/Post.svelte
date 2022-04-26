f014d36c3fb7e4b7b19f291df074144203616093
artkalugin <64751684+artkalugin@users.noreply.github.com>
2022-04-26 17:49:48 +0500

    Create Post.svelte

---
+++ src/Post.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let title;
+    export let author;
+    export let date;
+    export let content;
+</script>
+
+<h2>{title}</h2>
+<b>{author}</b>
+<em>{date}</em>
+<p>{content}</p>
