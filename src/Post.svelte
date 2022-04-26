74f28785b3f9f78fdb138a876f49d13676f66739
artkalugin <artkalugin27@gmail.com>
2022-04-26 20:49:01 +0500

    foolprofing

--- src/Post.svelte
+++ src/Post.svelte
@@ -5,7 +5,7 @@
     export let content;
 </script>
 
-<h2>{title}</h2>
-<b>{author}</b>
-<em>{date}</em>
-<p>{content}</p>
+<h2>{title || 'Untitled post'}</h2>
+<b>{author || 'Unknown author'}</b>
+<em>{date || new Date().toDateString()}</em>
+<p>{content || 'This post has no content'}</p>
